7156b0827513e585b58065e1078ca9d3753d89ab
kurinto <aika.miura@gmail.com>
2017-06-09 16:08:45 -0400

    toh-pt5 complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,31 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
+import { AppRoutingModule }   from './app-routing.module';
 import { AppComponent }  from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent } from './heroes.component';
+import { DashboardComponent } from './dashboard.component';
+
+import { HeroService } from './hero.service';
 
 @NgModule({
   imports:      [ 
-  	BrowserModule, FormsModule 
+  	BrowserModule, 
+  	FormsModule,
+    AppRoutingModule 
   ],
   declarations: [ 
   	AppComponent,
-  	HeroDetailComponent 
+  	HeroDetailComponent,
+  	HeroesComponent,
+    DashboardComponent 
   ],
-  bootstrap:    [ AppComponent ]
+  providers:    [ 
+  	HeroService 
+  ],
+  bootstrap:   [ AppComponent ]
+
+
 })
 export class AppModule { }
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -12,10 +12,15 @@ export class HeroService {
   }
 
   getHeroesSlowly(): Promise<Hero[]> {
-	  return new Promise(resolve => {
-	    // Simulate server latency with 2 second delay
-	    setTimeout(() => resolve(this.getHeroes()), 2000);
-  });
-}
+		return new Promise(resolve => {
+		    // Simulate server latency with 2 second delay
+		    setTimeout(() => resolve(this.getHeroes()), 2000);
+	  });
+	}
+
+  getHero(id: number): Promise<Hero> {
+    return this.getHeroes()
+            .then(heroes => heroes.find(hero => hero.id === id));
+  }
 
 }
